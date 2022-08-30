989f66b481349070faf9f8f0ad2774b3c73913a4
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-08-31 00:09:00 +0300

    onClick added

--- js/index.js
+++ js/index.js
@@ -3,3 +3,31 @@
 const slidebtn = document.getElementById("slide");
 const backbtn = document.getElementById("backBtn");
 const nextbtn = document.getElementById("nextBtn");
+
+const sliderImg = new Array(
+    "images/img1.jpg",
+    "images/img2.jpg",
+    "images/img3.jpg",
+    "images/img4.jpg",
+
+);
+
+let i = 0;
+
+nextbtn.onclick = function() {
+   if (i < 3){
+    slide.src = sliderImg[i+1];
+    
+    i++;
+
+   } 
+}
+
+backbtn.onclick = function() {
+    if (i > 0){
+     slide.src = sliderImg[i+1];
+     
+     i--;
+ 
+    } 
+ }
