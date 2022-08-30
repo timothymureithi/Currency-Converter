ab7699e581f377653d8b573e36ba3ec45fa35d41
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-08-30 15:53:17 +0300

    Declaring varibles for button functionality

---
+++ js/index.js
@@ -0,0 +1,5 @@
+//functionality for back and next button
+
+const slidebtn = document.getElementById("slide");
+const backbtn = document.getElementById("backBtn");
+const nextbtn = document.getElementById("nextBtn");
