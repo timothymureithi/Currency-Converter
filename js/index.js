14e6c2cad9f3ddbaec7bee5aad74fd2044500c2d
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-08-31 16:57:58 +0300

    fetch request from exchange-rate api

--- js/index.js
+++ js/index.js
@@ -39,7 +39,7 @@ backbtn.onclick = function() {
  const currency2 = document.getElementById("currency-2")
  const amount1 = document.getElementById('amount-one')
  const amount2 = document.getElementById('amount-two')
- const rate = document.getElementById('rate')
+ const Convrate = document.getElementById('rate')
  const swapbutton = document.getElementById('swap')
 
  /*Define an change event listeners. */
@@ -52,4 +52,23 @@ backbtn.onclick = function() {
  function calculate(){
     const currency_1 = currency1.value;
     const currency_2 = currency2.value; 
+
+    fetch(`https://v6.exchangerate-api.com/v6/c50c0dd18c871c23de59c4e4/latest/${currency_1}`)
+    .then(response => response.json())
+    .then((data) => {
+        //  console.log(data);
+        const rate = data.conversion_rates[currency_2];
+        Convrate.innerText = `0 ${currency_1} = ${rate} ${currency_2}`;
+        amount2.value = (amount1.value * rate).toFixed(2);
+
+    });
  }
+
+ swap.addEventListener('click', () => {
+    const tempVar = currency1.value;
+    currency1.value = currency2.value;
+    currency2.value = tempVar;
+    calculate();
+
+ })
+
